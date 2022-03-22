a2bf081bc818c4c9a8f1bc422cec17d905cd74e2
Pedjpedj <pedjovensindol@gmail.com>
2022-03-22 13:47:04 +0800

    fix setup

--- setup.js
+++ setup.js
@@ -3,177 +3,178 @@ var fs = require('fs-extra');
 var log = require('npmlog');
 
 if (!fs.ensureFileSync('./src/config.json')) {
-    log.info('config', 'Configuration file found!');
+	log.info('config', 'Configuration file found!');
 }
 else {
-    log.info('config', 'Configuration file not found, creating one...');
-    fs.writeFileSync('./src/config.json', JSON.stringify(utils.defaultConfig, null, 4));
+	log.info('config', 'Configuration file not found, creating one...');
+	fs.writeFileSync('./src/config.json', JSON.stringify(utils.defaultConfig, null, 4));
 }
 
 var config = require('./src/config.json');
-var log = require('npmlog');
 var inquirer = require('inquirer');
 
 function checkIfEmpty(value) {
-    if (value.length === 0) {
-        return 'This field is required';
-    }
-    return true;
+	if (value.length === 0) {
+		return 'This field is required';
+	}
+	return true;
 }
 
 function checkIfEmail(value) {
-    if (!value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-        return 'Please enter a valid email address';
-    }
-    return true;
+	if (!value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+		return 'Please enter a valid email address';
+	}
+	return true;
 }
 
 function checkIfThreadID(value) {
-    if (!value.match(/^\d+$/) || value.length !== 16) {
-        return 'Invalid thread ID';
-    }
-    return true;
+	if (!value.match(/^\d+$/) || value.length !== 16) {
+		return 'Invalid thread ID';
+	}
+	return true;
 }
 
 inquirer.prompt([{
-    type: 'list',
-    name: 'setup',
-    message: 'What do you want to setup?',
-    choices: ['Login', 'Config'],
+	type: 'list',
+	name: 'setup',
+	message: 'What do you want to setup?',
+	choices: ['Login', 'Config'],
 }]).then(answers => {
-    if (answers.setup === 'Login') {
-        inquirer.prompt([{
-                type: 'input',
-                name: 'email',
-                message: 'Enter your email address: ',
-                validate: checkIfEmail,
-            },
-            {
-                type: 'password',
-                name: 'password',
-                mask: '*',
-                message: 'Enter your password: ',
-                validate: checkIfEmpty,
-            }
-        ]).then(answers => {
-            utils.fetchCookie(answers.email, answers.password).then(() => {
-                inquirer.prompt([{
-                    type: 'confirm',
-                    name: 'setup',
-                    message: 'Do you want to setup the config file?',
-                    default: false,
-                }]).then(answers => {   
-                    if (answers.setup == true) {
-                        inquirer.prompt([
-                            {
-                                type: 'input',
-                                name: 'prefix',
-                                message: 'Enter the prefix:',
-                                default: '.',
-                            },
-                            {
-                                type: 'input',
-                                name: 'threadID',
-                                message: 'Enter your thread ID:',
-                                validate: checkIfThreadID,
-                            },
-                            {
-                                type: 'input',
-                                name: 'botName',
-                                message: 'Enter your bot name:',
-                                default: 'Bot',
-                            },
-                            {
-                                type: 'confirm',
-                                name: 'gcLock',
-                                message: 'Do you want to lock the GC?:',
-                                default: false,
-                            }
-                        ]).then(answers => {
-                            arg.prefix = answers.prefix;
-                            arg.threadID = answers.threadID;
-                            arg.botName = answers.botName;
-                            arg.gcLock = answers.gcLock;
-                            utils.writeConfig(arg);
-                        });
-                    }
-                    else return;
-                })
-            });
-        });
-    }
-    if (answers.setup === 'Config') {
-        inquirer.prompt([{
-            type: 'list',
-            name: 'config',
-            message: 'What do you want to configure?',
-            choices: ['Prefix', 'Bot name', 'Response', 'Thread ID', 'Weather API Key', 'GC Lock'],
-        }]).then(answers => {
-            if (answers.config === 'Prefix') {
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'prefix',
-                    message: 'Enter the prefix:',
-                    validate: checkIfEmpty,
-                }]).then(answers => {
-                    config.prefix = answers.prefix;
-                    writeConfig(config);
-                });
-            }
-            if (answers.config === 'Bot name') {
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'botName',
-                    message: 'Enter the bot\'s name:',
-                    validate: checkIfEmpty,
-                }]).then(answers => {
-                    config.botName = answers.botName;
-                    writeConfig(config);
-                });
-            }
-            if (answers.config === 'response') {
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'response',
-                    message: 'Enter the response:',
-                }]).then(answers => {
-                    config.response = answers.response;
-                    writeConfig(config);
-                });
-            }
-            if (answers.config === 'Thread ID') {
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'threadID',
-                    message: 'Enter the thread ID:',
-                    validate: checkIfThreadID,
-                }]).then(answers => {
-                    config.threadID = answers.threadID;
-                    writeConfig(config);
-                });
-            }
-            if (answers.config === 'Weather API Key') {
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'weatherAPIKey',
-                    message: 'Enter the weather API key:',
-                }]).then(answers => {
-                    config.weatherAPIKey = answers.weatherAPIKey;
-                    writeConfig(config);
-                });
-            }
-            if (answers.config === 'GC Lock') {
-                inquirer.prompt([{
-                    type: 'confirm',
-                    name: 'gcLock',
-                    message: 'Do you want to lock the GC?:',
-                    default: false,
-                }]).then(answers => {
-                    config.gcLock = answers.gcLock;
-                    writeConfig(config);
-                });
-            }
-        });
-    }
-}) // obviously, this is not the best way to do this, but it works for now
+	if (answers.setup === 'Login') {
+		inquirer.prompt([{
+				type: 'input',
+				name: 'email',
+				message: 'Enter your email address: ',
+				validate: checkIfEmail,
+			},
+			{
+				type: 'password',
+				name: 'password',
+				mask: '*',
+				message: 'Enter your password: ',
+				validate: checkIfEmpty,
+			}
+		]).then(answers => {
+			utils.fetchCookie(answers.email, answers.password).then(() => {
+				inquirer.prompt([{
+					type: 'confirm',
+					name: 'setup',
+					message: 'Do you want to setup the config file?',
+					default: false,
+				}]).then(answers => {   
+					if (answers.setup == true) {
+						inquirer.prompt([
+							{
+								type: 'input',
+								name: 'prefix',
+								message: 'Enter the prefix:',
+								default: '.',
+							},
+							{
+								type: 'input',
+								name: 'threadID',
+								message: 'Enter your thread ID:',
+								validate: checkIfThreadID,
+							},
+							{
+								type: 'input',
+								name: 'botName',
+								message: 'Enter your bot name:',
+								default: 'Bot',
+							},
+							{
+								type: 'confirm',
+								name: 'gcLock',
+								message: 'Do you want to lock the GC?:',
+								default: false,
+							}
+						]).then(answers => {
+							let arg;
+							arg.prefix = answers.prefix;
+							arg.threadID = answers.threadID;
+							arg.botName = answers.botName;
+							arg.gcLock = answers.gcLock;
+							utils.writeConfig(arg);
+						});
+					}
+					else return;
+				})
+			});
+		});
+	}
+	if (answers.setup === 'Config') {
+		inquirer.prompt([{
+			type: 'list',
+			name: 'config',
+			message: 'What do you want to configure?',
+			choices: ['Prefix', 'Bot name', 'Response', 'Thread ID', 'Weather API Key', 'GC Lock'],
+		}]).then(answers => {
+			if (answers.config === 'Prefix') {
+				inquirer.prompt([{
+					type: 'input',
+					name: 'prefix',
+					message: 'Enter the prefix:',
+					validate: checkIfEmpty,
+				}]).then(answers => {
+					config.prefix = answers.prefix;
+					utils.writeConfig(config);
+				});
+			}
+			if (answers.config === 'Bot name') {
+				inquirer.prompt([{
+					type: 'input',
+					name: 'botName',
+					message: 'Enter the bot\'s name:',
+					validate: checkIfEmpty,
+				}]).then(answers => {
+					config.botName = answers.botName;
+					utils.writeConfig(config);
+				});
+			}
+			if (answers.config === 'response') {
+				inquirer.prompt([{
+					type: 'input',
+					name: 'response',
+					message: 'Enter the response:',
+				}]).then(answers => {
+					config.response = answers.response;
+					utils.writeConfig(config);
+				});
+			}
+			if (answers.config === 'Thread ID') {
+				inquirer.prompt([{
+					type: 'input',
+					name: 'threadID',
+					message: 'Enter the thread ID:',
+					validate: checkIfThreadID,
+				}]).then(answers => {
+					config.threadID = answers.threadID;
+					utils.writeConfig(config);
+				});
+			}
+			if (answers.config === 'Weather API Key') {
+				inquirer.prompt([{
+					type: 'input',
+					name: 'weatherAPIKey',
+					message: 'Enter the weather API key:',
+				}]).then(answers => {
+					config.weatherAPIKey = answers.weatherAPIKey;
+					utils.writeConfig(config);
+				});
+			}
+			if (answers.config === 'GC Lock') {
+				inquirer.prompt([{
+					type: 'confirm',
+					name: 'gcLock',
+					message: 'Do you want to lock the GC?:',
+					default: false,
+				}]).then(answers => {
+					config.gcLock = answers.gcLock;
+					utils.writeConfig(config);
+				});
+			}
+		});
+	}
+}) 
+// obviously, this is not the best way to do this, but it works for now
 // TODO: make this better
--- src/utils.js
+++ src/utils.js
@@ -1,194 +1,200 @@
 var fs = require('fs-extra');
 var log = require('npmlog');
 var puppeteer = require('puppeteer');
-var moment = require('moment');
 
 module.exports = {
-    defaultConfig: {
-        prefix: '!',
-        botName: 'bot',
-        response: 'Hello!',
-        threadID: '',
-        weatherAPIKey: '',
-        gcLock: false,
-    },
+	defaultConfig: {
+		prefix: '!',
+		botName: 'bot',
+		response: 'Hello!',
+		threadID: '',
+		weatherAPIKey: '',
+		gcLock: false,
+	},
 
-    // Reactions for the bot
-    successReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('✅', msgId);
-        });
-    },
+	// Reactions for the bot
+	successReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('✅', msgId);
+		});
+	},
 
-    errorReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('❌', msgId);
-        });
-    },
+	errorReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('❌', msgId);
+		});
+	},
 
-    dislikeReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('👎', msgId);
-        });
-    },
+	dislikeReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('👎', msgId);
+		});
+	},
 
-    likeReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('👍', msgId);
-        });
-    },
+	likeReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('👍', msgId);
+		});
+	},
 
-    lookReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('🔍', msgId);
-        });
-    },
+	lookReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('🔍', msgId);
+		});
+	},
 
-    eyesReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('👀', msgId);
-        });
-    },
+	eyesReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('👀', msgId);
+		});
+	},
 
-    noticeReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('❗', msgId);
-        });
-    },
+	noticeReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('❗', msgId);
+		});
+	},
 
-    sadReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-            api.setMessageReaction('😢', msgId);
-        });
-    },
+	sadReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('😢', msgId);
+		});
+	},
 
-    emptyReact: function (api, msgId) {
-        api.setMessageReaction('', msgId, (err) => {
-            if (err) return log.error(err);
-        });
-    },
+	emptyReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+		});
+	},
 
-    numberBulletGiver: function (arr) {
-        let txtArr = [];
-        for (let i = 0; i < arr.length; i++) {
-            txtArr.push((i + 1) + '. ' + arr[i]);
-        }
-        return txtArr.join('\n');
-    },
+	waitReact: function (api, msgId) {
+		api.setMessageReaction('', msgId, (err) => {
+			if (err) return log.error(err);
+			api.setMessageReaction('⏳', msgId);
+		});
+	},
 
-    // Returns a random number between min (inclusive) and max (exclusive)
-    getRandomInt: function (min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    },
+	numberBulletGiver: function (arr) {
+		let txtArr = [];
+		for (let i = 0; i < arr.length; i++) {
+			txtArr.push((i + 1) + '. ' + arr[i]);
+		}
+		return txtArr.join('\n');
+	},
 
-    // Returns a random element from an array
-    getRandomElement: function (arr) {
-        return arr[Math.floor(Math.random() * arr.length)];
-    },
+	// Returns a random number between min (inclusive) and max (exclusive)
+	getRandomInt: function (min, max) {
+		return Math.floor(Math.random() * (max - min)) + min;
+	},
 
-    sleep: function (ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    },
+	// Returns a random element from an array
+	getRandomElement: function (arr) {
+		return arr[Math.floor(Math.random() * arr.length)];
+	},
 
-    trim: function (str, max) {
-        return (str.length > max) ? str.substr(0, max - 3) + '...' : str;
-    },
+	sleep: function (ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	},
 
-    // Function that split a message into chunks of max length
-    splitMessage: function (message, maxLength) {
-        if (message.length <= maxLength) return [message];
+	trim: function (str, max) {
+		return (str.length > max) ? str.substr(0, max - 3) + '...' : str;
+	},
 
-        const split = message.split(' ');
-        const result = [];
-        let tempMsg = '';
+	// Function that split a message into chunks of max length
+	splitMessage: function (message, maxLength) {
+		if (message.length <= maxLength) return [message];
 
-        for (let i = 0; i < split.length; i++) {
-            if (tempMsg.length + split[i].length < maxLength) {
-                tempMsg += `${split[i]} `;
-            } else {
-                result.push(tempMsg);
-                tempMsg = '';
-                i--;
-            }
-        }
-        result.push(tempMsg);
+		const split = message.split(' ');
+		const result = [];
+		let tempMsg = '';
 
-        return result;
-    },
+		for (let i = 0; i < split.length; i++) {
+			if (tempMsg.length + split[i].length < maxLength) {
+				tempMsg += `${split[i]} `;
+			} else {
+				result.push(tempMsg);
+				tempMsg = '';
+				i--;
+			}
+		}
+		result.push(tempMsg);
 
-    writeConfig: function(arg) {
-        config = arg;
-        fs.writeFile('./src/config.json', JSON.stringify(config, null, 4), (err) => {
-            if (err) return log.error(err);
-            log.info('config', 'Configuration file updated!');
-        });
-    },
+		return result;
+	},
 
-    fetchCookie: async function (email, password) {
-        if (email === undefined || password === undefined) {
-            return log.error('Please provide an email and password');
-        }
+	writeConfig: function(arg) {
+		let config = arg;
+		fs.writeFile('./src/config.json', JSON.stringify(config, null, 4), (err) => {
+			if (err) return log.error(err);
+			log.info('config', 'Configuration file updated!');
+		});
+	},
 
-        if (email.length < 1 || password.length < 1) {
-            return log.error('Please provide an email and password');
-        }
+	fetchCookie: async function (email, password) {
+		if (email === undefined || password === undefined) {
+			return log.error('Please provide an email and password');
+		}
 
-        if (!fs.existsSync(__dirname + '/data/fbCookies.json')) {
-            fs.ensureFileSync(__dirname + '/data/fbCookies.json');
-            log.info('cookies', 'Created fb_cookies.json');
-        }
+		if (email.length < 1 || password.length < 1) {
+			return log.error('Please provide an email and password');
+		}
 
-        try {
-            log.info('cookies', 'Attempting to launch browser...');
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.setDefaultNavigationTimeout(60000);
-            await page.goto(`https://www.facebook.com/`);
-            await page.waitForSelector('#email');
-            try {
-                await page.type(`#email`, email);
-            } catch (err) {
-                return log.error('cookies', 'Error typing email');
-            }
+		if (!fs.existsSync(__dirname + '/data/fbCookies.json')) {
+			fs.ensureFileSync(__dirname + '/data/fbCookies.json');
+			log.info('cookies', 'Created fb_cookies.json');
+		}
 
-            try {
-                await page.type(`#pass`, password);
-            } catch (err) {
-                return log.error('cookies', 'Error typing password');
-            }
+		try {
+			log.info('cookies', 'Attempting to launch browser...');
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+			await page.setDefaultNavigationTimeout(60000);
+			await page.goto(`https://www.facebook.com/`);
+			await page.waitForSelector('#email');
+			try {
+				await page.type(`#email`, email);
+			} catch (err) {
+				return log.error('cookies', 'Error typing email');
+			}
 
-            log.info('cookies', 'Credentials entered, attempting to login...');
-            await page.click('[type="submit"]');
-            await page.waitForNavigation();
-            try {
-                await page.click('div');
+			try {
+				await page.type(`#pass`, password);
+			} catch (err) {
+				return log.error('cookies', 'Error typing password');
+			}
 
-            } catch (error) {
-                log.error('cookies', 'Error clicking div... Ignoring...');
-            }
+			log.info('cookies', 'Credentials entered, attempting to login...');
+			await page.click('[type="submit"]');
+			await page.waitForNavigation();
+			try {
+				await page.click('div');
 
-            cookies = await page.cookies();
-            cookies = cookies.map(({
-                    name: key,
-                    ...rest
-            }) => ({
-                    key,
-                    ...rest
-            }));
-            fs.writeFileSync(__dirname + '/data/fbCookies.json', JSON.stringify(cookies, null, 4));
-            log.info('cookies', 'Cookies saved!');
-            await browser.close();
-        } catch (err) {
-            log.error('cookies', 'Error:', err);
-            return;
-        }
-    },    
+			} catch (error) {
+				log.error('cookies', 'Error clicking div... Ignoring...');
+			}
+
+			let cookies = await page.cookies();
+			cookies = cookies.map(({
+					name: key,
+					...rest
+			}) => ({
+					key,
+					...rest
+			}));
+			fs.writeFileSync(__dirname + '/data/fbCookies.json', JSON.stringify(cookies, null, 4));
+			log.info('cookies', 'Cookies saved!');
+			await browser.close();
+		} catch (err) {
+			log.error('cookies', 'Error:', err);
+			return;
+		}
+	},    
 }
